b5db81c910a59d3c1b0d580639d3adc33fdc2c80
Felipe Spirandelli <70784142+FelipeSpirandelli@users.noreply.github.com>
2021-05-14 16:56:52 -0300

    feat: add image and price to product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Text, View, Button } from 'react-native'
+import { Text, View, Button, Image } from 'react-native'
 
 import Style from '../Styles/ProductsStyle.js'
 
-const Product = ({name , id, isAdded, toggleTask}) => {
+const Product = ({name , id, front_shiny, base_experience, isAdded, toggleTask}) => {
 
     return (
         <View style = {Style.textContainer}>
+            <Image 
+            style = {Style.image}
+            source= {{uri: front_shiny}}/>
             <Text style = {Style.primaryText}>{ name }</Text>
+            <Text style = {Style.primaryText}>R${ base_experience }</Text>
             <Button
                 title={ isAdded ?
                     "Retirar" :
